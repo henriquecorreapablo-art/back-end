348fbb0831209ebd529a9efb777fda1b710353ae
henriquecorreapablo-art <henrique.correa.pablo@escola.pr.gov.br>
2026-03-16 10:41:19 -0300

    Create cont05.py

---
+++ conteúdos/cont05.py
@@ -0,0 +1,18 @@
+# --- EXEMPLO ---
+
+# 1. Multiplicação vem antes da soma
+# Cálculo: 5 + (3 * 2) = 5 + 6 = 11
+print(5 + 3 * 2) 
+
+# 2. Potenciação (**) vem antes da multiplicação (*) e da soma (+)
+# Cálculo: (3 * 5) + (4 ** 2) = 15 + 16 = 31
+print(3 * 5 + 4 ** 2)
+
+# 3. Parênteses têm prioridade máxima, depois a potência
+# Cálculo: 3 * (9 ** 2) = 3 * 81 = 243
+print(3 * (5 + 4) ** 2)
+
+# Observação teórica:
+# O Python é utilizado em cálculos científicos devido à sua capacidade 
+# de lidar com o tamanho da memória de forma dinâmica, ou seja, 
+# conseguimos chegar a números extremamente grandes (inteiros de precisão arbitrária).
