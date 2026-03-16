9f18fb568f81c97ea36b980274bfc662ad97349c
henriquecorreapablo-art <henrique.correa.pablo@escola.pr.gov.br>
2026-03-16 10:58:16 -0300

    Create ex006.py

---
+++ exercicios/ex006.py
@@ -0,0 +1,9 @@
+# Solicita um número inteiro ao usuário
+n = int(input('Digite um número: '))
+
+# Calcula o antecessor e o sucessor
+ant = n - 1
+suc = n + 1
+
+# Exibe o resultado na tela
+print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n, ant, suc))
