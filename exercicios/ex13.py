ab3a77f29d6bcee2e0debff488cbcde68c32fff9
henriquecorreapablo-art <henrique.correa.pablo@escola.pr.gov.br>
2026-03-23 10:37:44 -0300

    Create ex13.py

---
+++ exercicios/ex13.py
@@ -0,0 +1,4 @@
+preço = float(input('Qual é o preço do produto? R$'))
+novo_preço = preço - (preço * 5 / 100)
+
+print('O produto que custava R${}, com 5% de desconto vai custar R${:.2f}'.format(preço, novo_preço))
