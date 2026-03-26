c6a5c23df5d131581ec29a91fec3894dfccfbdac
henriquecorreapablo-art <henrique.correa.pablo@escola.pr.gov.br>
2026-03-26 08:45:24 -0300

    Update cont8.py

--- conteúdos/cont8.py
+++ conteúdos/cont8.py
@@ -3,3 +3,8 @@ num = int(input('Digite um número: ' ))
 raiz = math.sqrt(num)
 print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
 print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
+import math
+num = int(input('Digite um número: ' ))
+raiz = math.sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
